067c2ee4b0284a33f60cee0b242ceaa603ab8d31
waonpad <waonpad@gmail.com>
2024-09-25 21:06:22 +0900

    修正

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,11 @@ import { PHASE_DEVELOPMENT_SERVER } from "next/constants.js";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = (phase, { defaultConfig }) => {
+  /**
+   * NOTICE: ローカルでは GITHUB_REPOSITORY が設定されていないため 空文字 になる
+   *
+   * [変数に情報を保存する - GitHub ドキュメント](https://docs.github.com/ja/actions/writing-workflows/choosing-what-your-workflow-does/store-information-in-variables#default-environment-variables)
+   */
   const basePath = process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY.split("/")[1]}` : "";
 
   return {
@@ -16,10 +21,6 @@ const nextConfig = (phase, { defaultConfig }) => {
     experimental: {
       typedRoutes: true,
     },
-    // NOTICE: ローカルでは GITHUB_REPOSITORY が設定されていないため 空文字 になる
-    /**
-     * [変数に情報を保存する - GitHub ドキュメント](https://docs.github.com/ja/actions/writing-workflows/choosing-what-your-workflow-does/store-information-in-variables#default-environment-variables)
-     */
     basePath,
     pageExtensions: ["ts", "tsx", "js", "jsx"].flatMap((extension) => {
       const isDevServer = phase === PHASE_DEVELOPMENT_SERVER;
