1ec4e9d07b408483528f2b2e208666cd57033e7b
asai <asai@fenrir.co.jp>
2024-10-01 16:54:34 +0900

    a

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,7 +4,7 @@ import { generateStaticParams as generateStaticArticleParams } from "./(frontend
 import { generateStaticParams as generateStaticTagParams } from "./(frontend)/tags/[tagCode]/page";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const baseUrl = clientEnv.NEXT_PUBLIC_SITE_URL;
+  const siteUrl = clientEnv.NEXT_PUBLIC_SITE_URL;
 
   const articleRoutes = (await generateStaticArticleParams()).map((params) => `/articles/${params.issueNumber}`);
   const tagRoutes = (await generateStaticTagParams()).map((params) => `/tags/${params.tagCode}`);
@@ -13,12 +13,12 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 
   return routes.map((route) => {
     if (typeof route === "string") {
-      return { url: `${baseUrl}${route}` };
+      return { url: `${siteUrl}${route}` };
     }
 
     return {
       ...route,
-      url: `${baseUrl}${route.url}`,
+      url: `${siteUrl}${route.url}`,
     };
   });
 }
