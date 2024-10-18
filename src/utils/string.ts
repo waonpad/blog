4c593a308024cd6ad9f23ce96855e9ef890b8dff
waonpad <waonpad@gmail.com>
2024-10-19 05:18:12 +0900

    不要になったので削除

--- src/utils/string.ts
+++
@@ -1,9 +0,0 @@
-export const getUniqueChars = (text: string): string => {
-  const charSet = new Set<string>();
-
-  for (const char of text) {
-    charSet.add(char);
-  }
-
-  return Array.from(charSet).join("");
-};
