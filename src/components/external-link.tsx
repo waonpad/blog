a1449d8b7aee30252ffc58e451451203189a3e41
waonpad <waonpad@gmail.com>
2024-12-06 22:51:15 +0900

    にゃーん

--- src/components/external-link.tsx
+++ src/components/external-link.tsx
@@ -2,9 +2,9 @@ import type { ComponentPropsWithoutRef } from "react";
 
 type Props = Omit<ComponentPropsWithoutRef<"a">, "rel" | "target">;
 
-export const ExternalLink = ({ children, className = "", ...rest }: Props) => {
+export const ExternalLink = ({ children, ...rest }: Props) => {
   return (
-    <a {...rest} rel="noopener noreferrer" target="_blank" className={`${className}`}>
+    <a {...rest} rel="noopener noreferrer" target="_blank">
       {children}
     </a>
   );
