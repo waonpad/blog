08e88b349a1a6199db9a74a55ac37642697aa096
waonpad <waonpad@gmail.com>
2025-04-07 21:12:28 +0900

    ref

--- src/app/(frontend)/articles/[issueNumber]/page.tsx
+++ src/app/(frontend)/articles/[issueNumber]/page.tsx
@@ -1,7 +1,9 @@
+import { ArticleList } from "@/components/article-list";
 import { Time } from "@/components/time";
 import { getIssue, listIssues } from "@/lib/issue";
 import { listIssueComments } from "@/lib/issue/comment";
-import { sortByKey } from "@/utils/sort";
+import { getIssueReferences } from "@/lib/issue/reference";
+import { sortByDateKey, sortByKey } from "@/utils/sort";
 import type { Metadata } from "next";
 import Link from "next/link";
 
@@ -48,6 +50,24 @@ export default async function Page(props: Props) {
 
   const labels = sortByKey(issue.labels.flat(), "name");
 
+  const issueReferences = getIssueReferences({ issueNumber });
+
+  const referencingIssues = await Promise.all(
+    issueReferences.referencings.map(
+      async (referencingIssueNumber) => await getIssue({ issueNumber: referencingIssueNumber }),
+    ),
+  );
+
+  const referencedIssues = await Promise.all(
+    issueReferences.referencedBy.map(
+      async (referencedIssueNumber) => await getIssue({ issueNumber: referencedIssueNumber }),
+    ),
+  );
+
+  const isReferencingIssuesExists = referencingIssues.length > 0;
+  const isReferencedIssuesExists = referencedIssues.length > 0;
+  const isAnyIssueReferenceExists = isReferencingIssuesExists || isReferencedIssuesExists;
+
   return (
     // コメントでの補足等を含めて1つの記事とする想定なため、最上位にarticle、その中はsectionにしている
     <article className="w-full divide-y divide-[#30363db3]">
@@ -79,6 +99,34 @@ export default async function Page(props: Props) {
           <div dangerouslySetInnerHTML={{ __html: issueComment.body_html_md }} />
         </section>
       ))}
+      {isAnyIssueReferenceExists && (
+        <section className="pt-8">
+          <header className="markdown mb-3!">
+            <h2>関連記事</h2>
+          </header>
+          <div className="flex flex-col gap-6">
+            {isReferencingIssuesExists && (
+              <section>
+                <header className="markdown mb-2!">
+                  <h3>本文中で参照した記事</h3>
+                </header>
+                <ArticleList articles={referencingIssues} className="gap-2" />
+              </section>
+            )}
+            {isReferencedIssuesExists && (
+              <section>
+                <header className="markdown mb-2!">
+                  <h3>この記事を参照している記事</h3>
+                </header>
+                <ArticleList
+                  articles={sortByDateKey(referencedIssues, "created_at", { order: "desc" })}
+                  className="gap-2"
+                />
+              </section>
+            )}
+          </div>
+        </section>
+      )}
     </article>
   );
 }
