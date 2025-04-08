9368265915f0841d9300d2bf55da787b2ab74819
waonpad <waonpad@gmail.com>
2025-04-08 17:12:33 +0900

    ^^

--- src/lib/issue/markdown.ts
+++ src/lib/issue/markdown.ts
@@ -21,83 +21,83 @@ import { unified } from "unified";
  * @see [remarkjs/remark](https://github.com/remarkjs/remark)
  */
 export const renderMarkdown = async (markdown: string): Promise<string> => {
-  const result = await unified()
-    .use(remarkParse)
-    .use(remarkHtml)
-    .use(remarkGfm)
-    .use(remarkGithub, {
-      // 今回の使用方法ではリポジトリ名は使用しないため適当なものを指定
-      repository: "user/repo",
-      buildUrl: (values) => {
-        // Issueの場合のみ記事リンクとして扱う
-        if (values.type === "issue") {
-          // articles/[issueNumber]/ にリンクする
-          // GitHub Pagesの場合は末尾にスラッシュを付けるため、リンクの末尾にもスラッシュを付ける
-          // next.config.ts の trailingSlash の値に合わせる
-          return `${clientEnv.NEXT_PUBLIC_SITE_URL}/articles/${values.no}/`;
-        }
+  return (
+    await unified()
+      .use(remarkParse)
+      .use(remarkHtml)
+      .use(remarkGfm)
+      .use(remarkGithub, {
+        // 今回の使用方法ではリポジトリ名は使用しないため適当なものを指定
+        repository: "user/repo",
+        buildUrl: (values) => {
+          // Issueの場合のみ記事リンクとして扱う
+          if (values.type === "issue") {
+            // articles/[issueNumber]/ にリンクする
+            // GitHub Pagesの場合は末尾にスラッシュを付けるため、リンクの末尾にもスラッシュを付ける
+            // next.config.ts の trailingSlash の値に合わせる
+            return `${clientEnv.NEXT_PUBLIC_SITE_URL}/articles/${values.no}/`;
+          }
 
-        // Issue以外のリンクを書く事は無いはずだが、もし書かれていてもリンクにしない
-        return false;
-      },
-    })
-    .use(remarkRehype, { allowDangerousHtml: true })
-    // YouTubeの埋め込み等をするためのやつ
-    .use(rehypeRaw)
-    // コードブロックのシンタックスハイライト
-    .use(rehypePrettyCode, {
-      theme: "github-dark",
-      keepBackground: false,
-      transformers: [
-        // コードブロックのコピー機能
-        transformerCopyButton({
-          visibility: "always",
-          feedbackDuration: 3_000,
-        }),
-      ],
-    })
-    // 外部リンクを新しいタブで開く
-    .use(rehypeExternalLinks, {
-      target: "_blank",
-      rel: ["noopener", "noreferrer"],
-    })
-    .use(rehypeSlug)
-    // 目次を生成
-    .use(rehypeToc, {
-      customizeTOC: (toc) => {
-        // @ts-ignore
-        const items = (toc.children?.[0].children || []) as Node[];
-        // 見出しの数が0の場合は目次を表示しない
-        if (items.length === 0) return false;
+          // Issue以外のリンクを書く事は無いはずだが、もし書かれていてもリンクにしない
+          return false;
+        },
+      })
+      .use(remarkRehype, { allowDangerousHtml: true })
+      // YouTubeの埋め込み等をするためのやつ
+      .use(rehypeRaw)
+      // コードブロックのシンタックスハイライト
+      .use(rehypePrettyCode, {
+        theme: "github-dark",
+        keepBackground: false,
+        transformers: [
+          // コードブロックのコピー機能
+          transformerCopyButton({
+            visibility: "always",
+            feedbackDuration: 3_000,
+          }),
+        ],
+      })
+      // 外部リンクを新しいタブで開く
+      .use(rehypeExternalLinks, {
+        target: "_blank",
+        rel: ["noopener", "noreferrer"],
+      })
+      .use(rehypeSlug)
+      // 目次を生成
+      .use(rehypeToc, {
+        customizeTOC: (toc) => {
+          // @ts-ignore
+          const items = (toc.children?.[0].children || []) as Node[];
+          // 見出しの数が0の場合は目次を表示しない
+          if (items.length === 0) return false;
 
-        // 開いたり閉じたりできるようにする
-        const wrappedToc = {
-          type: "element",
-          tagName: "details",
-          children: [
-            {
-              type: "element",
-              tagName: "summary",
-              children: [{ type: "text", value: "目次" }],
-            },
-            toc,
-          ],
-        };
+          // 開いたり閉じたりできるようにする
+          const wrappedToc = {
+            type: "element",
+            tagName: "details",
+            children: [
+              {
+                type: "element",
+                tagName: "summary",
+                children: [{ type: "text", value: "目次" }],
+              },
+              toc,
+            ],
+          };
 
-        return wrappedToc;
-      },
-    })
-    // 見出しにリンクを追加
-    .use(rehypeAutolinkHeadings, {
-      behavior: "wrap",
-      properties: {
-        className: "alternative-link",
-        // tailwindによるスタイルの敵用がされない可能性があるため、直接styleを指定
-        style: "display: inline-block; width: 100%;",
-      },
-    })
-    .use(rehypeStringify)
-    .process(markdown);
-
-  return result.toString();
+          return wrappedToc;
+        },
+      })
+      // 見出しにリンクを追加
+      .use(rehypeAutolinkHeadings, {
+        behavior: "wrap",
+        properties: {
+          className: "alternative-link",
+          // tailwindによるスタイルの敵用がされない可能性があるため、直接styleを指定
+          style: "display: inline-block; width: 100%;",
+        },
+      })
+      .use(rehypeStringify)
+      .process(markdown)
+  ).toString();
 };
