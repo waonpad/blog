2e08b6de76e10989e6931064338171d4500fd1d6
waonpad <waonpad@gmail.com>
2025-04-08 21:05:13 +0900

    ?

--- src/lib/issue/transform.ts
+++ src/lib/issue/transform.ts
@@ -1,31 +1,5 @@
-import { labelCodeSeparator } from "./config";
 import type { GHLabel, Label } from "./types";
-
-/**
- * @example
- * extractLabelCode("label___description") // "label"
- */
-const extractLabelCode = (labelDescription: string): string | null => {
-  if (!labelDescription.includes(labelCodeSeparator)) {
-    return null;
-  }
-
-  const code = labelDescription.split(labelCodeSeparator)[0];
-
-  return code === "" ? null : code;
-};
-
-/**
- * @example
- * extractDisplayLabelDescription("label___description") // "description"
- */
-const extractDisplayLabelDescription = (labelDescription: string): string | null => {
-  if (!labelDescription.includes(labelCodeSeparator)) {
-    return labelDescription;
-  }
-
-  return labelDescription.split(labelCodeSeparator)[1] || null;
-};
+import { extractDisplayLabelDescription, extractLabelCode } from "./utils";
 
 /**
  * プロジェクト内でラベルを扱うために加工処理をする
---
+++ src/lib/issue/utils.ts
@@ -0,0 +1,25 @@
+import { labelCodeSeparator } from "./config";
+
+/**
+ * @example
+ * extractLabelCode("label___description") // "label"
+ */
+export const extractLabelCode = (labelDescription: string): string | null => {
+  if (!labelDescription.includes(labelCodeSeparator)) {
+    return null;
+  }
+
+  const code = labelDescription.split(labelCodeSeparator)[0];
+
+  return code === "" ? null : code;
+};
+
+/**
+ * @example
+ * extractDisplayLabelDescription("label___description") // "description"
+ */
+export const extractDisplayLabelDescription = (labelDescription: string): string | null => {
+  if (!labelDescription.includes(labelCodeSeparator)) return labelDescription;
+
+  return labelDescription.split(labelCodeSeparator)[1] || null;
+};
